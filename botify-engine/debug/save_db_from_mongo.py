5c4a7df1892275dcbc88d0dd03212795994480ed
Angus <1621xjx@buaa.edu.cn>
2018-08-01 16:55:39 +0800

    fix encoding bug

--- botify-engine/debug/save_db_from_mongo.py
+++ botify-engine/debug/save_db_from_mongo.py
@@ -6,7 +6,7 @@ from urllib import parse
 passwd = "datiantian123!@#"
 passwd = parse.quote(passwd)  # 对密码先进行编码
 mango_uri = 'mongodb://%s:%s@%s:%s/%s' % ("kegger", passwd, "166.111.7.173", "30019", "admin")
-client = pymongo.MongoClient(mango_uri)
+client = pymongo.MongoClient(mango_uri, unicode_decode_error_handler='ignore')
 
 # 操作test数据库
 db_name = 'bigsci'
@@ -19,14 +19,13 @@ collection_set01 = db['publication_dupl']
 
 dic = {}
 for i, r in enumerate(collection_set01.find()):
-    if i <= 41400 or i >= 42400:
         if r['lang'] == 'en' and 'venue' in r.keys():
             if 'raw' in r['venue'].keys():
                 if r['venue']['raw'] in dic.keys():
                     dic[r['venue']['raw']] += 1
                 else:
                     dic[r['venue']['raw']] = 1
-        print(r['_id'])
+        print(str(r['_id']) + ' ' + str(i))
 
 out_file = open("../data/lexicons/bigsci_venues.txt", "w", encoding="utf8")
 for key in dic.keys():
